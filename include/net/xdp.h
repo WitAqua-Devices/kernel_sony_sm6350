0a40c3e0576388d6759e11eb280f1c627418d178
Jesper Dangaard Brouer <brouer@redhat.com>
2020-05-14 12:49:02 +0200

    UPSTREAM: xdp: Add frame size to xdp_buff
    XDP have evolved to support several frame sizes, but xdp_buff was not
    updated with this information. The frame size (frame_sz) member of
    xdp_buff is introduced to know the real size of the memory the frame is
    delivered in.
    
    When introducing this also make it clear that some tailroom is
    reserved/required when creating SKBs using build_skb().
    
    It would also have been an option to introduce a pointer to
    data_hard_end (with reserved offset). The advantage with frame_sz is
    that (like rxq) drivers only need to setup/assign this value once per
    NAPI cycle. Due to XDP-generic (and some drivers) it's not possible to
    store frame_sz inside xdp_rxq_info, because it's varies per packet as it
    can be based/depend on packet length.
    
    V2: nitpick: deduct -> deduce
    
    Change-Id: I48a37cf591c48c439c33e5841d5d703b6408a35f
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Link: https://lore.kernel.org/bpf/158945334261.97035.555255657490688547.stgit@firesoul
    (cherry picked from commit a3d4e74d864437273d539c28f86ecbb30a9a2a69)

--- include/net/xdp.h
+++ include/net/xdp.h
@@ -6,6 +6,8 @@
 #ifndef __LINUX_NET_XDP_H__
 #define __LINUX_NET_XDP_H__
 
+#include <linux/skbuff.h> /* skb_shared_info */
+
 /**
  * DOC: XDP RX-queue information
  *
@@ -77,6 +79,16 @@ struct xdp_buff {
 	struct xdp_txq_info *txq;
 	u32 frame_sz; /* frame size to deduce data_hard_end/reserved tailroom*/
 };
+
+/* Reserve memory area at end-of data area.
+ *
+ * This macro reserves tailroom in the XDP buffer by limiting the
+ * XDP/BPF data access to data_hard_end.  Notice same area (and size)
+ * is used for XDP_PASS, when constructing the SKB via build_skb().
+ */
+#define xdp_data_hard_end(xdp)				\
+	((xdp)->data_hard_start + (xdp)->frame_sz -	\
+	 SKB_DATA_ALIGN(sizeof(struct skb_shared_info)))
 
 struct xdp_frame {
 	void *data;

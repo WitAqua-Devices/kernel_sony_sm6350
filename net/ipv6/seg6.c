09b0781485477112b83439b49110cf81fc9de3cc
soralis0912 <soralis.nem@gmail.com>
2026-08-09 03:06:03 +0900

    seg6: Declare max_last_entry in seg6_validate_srh()
    Lost while resolving the merge of the slab-out-of-bounds fix.

--- net/ipv6/seg6.c
+++ net/ipv6/seg6.c
@@ -32,8 +32,9 @@
 
 bool seg6_validate_srh(struct ipv6_sr_hdr *srh, int len, bool reduced)
 {
-	int trailing;
 	unsigned int tlv_offset;
+	int max_last_entry;
+	int trailing;
 
 	if (srh->type != IPV6_SRCRT_TYPE_4)
 		return false;

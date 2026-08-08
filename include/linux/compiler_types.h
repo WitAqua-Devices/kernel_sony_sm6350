4fa8f51e6aec072cf6689afd90f0d973421eb570
soralis0912 <soralis.nem@gmail.com>
2026-08-09 03:08:19 +0900

    compiler_types: Add the fallthrough pseudo keyword
    The BPF verifier backports use it, and this tree predates
    compiler_attributes.h where upstream put it.

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -311,4 +311,20 @@ struct ftrace_likely_data {
  */
 #define noinline_for_stack noinline
 
+/*
+ * Add the pseudo keyword 'fallthrough' so case statement blocks
+ * must end with any of these keywords:
+ *   break;
+ *   fallthrough;
+ *   goto <label>;
+ *   return [expression];
+ *
+ *  gcc: https://gcc.gnu.org/onlinedocs/gcc/Statement-Attributes.html#Statement-Attributes
+ */
+#if __has_attribute(__fallthrough__)
+# define fallthrough                    __attribute__((__fallthrough__))
+#else
+# define fallthrough                    do {} while (0)  /* fallthrough */
+#endif
+
 #endif /* __LINUX_COMPILER_TYPES_H */

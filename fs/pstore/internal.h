560d5f784a9897b537cf3210e14055432a678c02
soralis0912 <soralis.nem@gmail.com>
2026-08-10 16:38:50 +0900

    Revert "pstore: Keep 128K of the log rather than 10K"
    This reverts commit cb45282c0f894df7ba6dd3f9ba1e1d78bd2b1e30.
    
    It said to drop it once pdx213 boots, and pdx213 boots: Android comes up
    to the lock screen and adb answers.  Nothing else should have to pay 128K
    of ramoops for a debugging session that is over - the dump zone is only
    512K, so at the default record size that was a third of it for one panic.

--- fs/pstore/internal.h
+++ fs/pstore/internal.h
@@ -6,7 +6,7 @@
 #include <linux/time.h>
 #include <linux/pstore.h>
 
-#define PSTORE_DEFAULT_KMSG_BYTES 131072
+#define PSTORE_DEFAULT_KMSG_BYTES 10240
 extern unsigned long kmsg_bytes;
 
 #ifdef CONFIG_PSTORE_FTRACE
